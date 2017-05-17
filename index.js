365fe6d68e0b5ddcaca5f00a7bcf5246a6cc0ccf
Peter Svitz <peter.svitz@gmail.com>
2017-05-16 22:39:04 -0400

    edit to push to heroic

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
-app.set('port', (process.env.PORT || 5000));
+app.use(express.static(path.join(__dirname, 'public'))); //  "public" off of current is root
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-
+app.listen(5000);
+console.log('Listening on port 5000');
---
+++ public/malicious.js
@@ -0,0 +1,1 @@
+alert('js from malicious.js');
